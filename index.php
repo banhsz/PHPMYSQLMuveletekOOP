4668ee85ff4b257375cb28eb34338a64f0d49fc3
Szabolcs Bánhidi <banhidi.szabolcs@petrik.hu>
2020-12-06 20:02:34 +0100

    Adatbázis osztály,konstruktor,kapcsolódás,kapcsolat bontás

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!-- Az adatbázist (pelda_adatbazis.db) importáljuk a tesztelés előtt! -->
 <!DOCTYPE html>
 <html lang="hu">
 <head>
@@ -6,6 +7,42 @@
     <title>Felhasználók</title>
 </head>
 <body>
-    
+    <?php
+    class Adatbazis
+	{
+		private $servername;
+		private $username;
+		private $password;
+		private $dbname;
+		private $conn;
+		private $sql;
+		private $result;
+		private $row;
+
+		function __construct()
+		{
+            //ide írjuk az adatbázis nevét és a kapcsolódási adatokat
+			$this->servername = "localhost";
+			$this->username = "root";
+			$this->password = "";
+			$this->dbname = "pelda_adatbazis";
+		}
+		//Adatbázis kapcsoloat nyitása
+		function kapcsolatNyitasa()
+		{
+			$this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+			if ($this->conn->connect_error)
+			{
+		  		die("Connection failed: " . $this->conn->connect_error);
+			}
+        }
+        //Adatbázis kapcsolat bontás
+		function kapcsolatBontasa()
+		{
+
+			$this->conn->close();
+		}
+    }
+    ?>
 </body>
 </html>
