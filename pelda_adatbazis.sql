cc8134a973d855da3bfddb50164a9d423498eaee
Szabolcs Banhidi <71526545+bstkozik@users.noreply.github.com>
2020-12-06 20:05:09 +0100

    MYSQL adatbázis
    Importájuk az adatbázist a használat előtt!

---
+++ pelda_adatbazis.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2020. Dec 06. 19:54
+-- Kiszolgáló verziója: 10.4.11-MariaDB
+-- PHP verzió: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `pelda_adatbazis`
+--
+create database pelda_adatbazis;
+use pelda_adatbazis;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `felhasznalok`
+--
+
+CREATE TABLE `felhasznalok` (
+  `id` int(11) NOT NULL,
+  `felhasznalonev` varchar(40) COLLATE utf8_hungarian_ci NOT NULL,
+  `jelszo` varchar(256) COLLATE utf8_hungarian_ci NOT NULL,
+  `e-mail` varchar(40) COLLATE utf8_hungarian_ci NOT NULL,
+  `jogosultsag` varchar(40) COLLATE utf8_hungarian_ci NOT NULL,
+  `aktiv` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `felhasznalok`
+--
+
+INSERT INTO `felhasznalok` (`id`, `felhasznalonev`, `jelszo`, `e-mail`, `jogosultsag`, `aktiv`) VALUES
+(1, 'Szabi', '12345', 'szabi@gmail.com', 'felhasználó', 1),
+(2, 'Robi', 'robi1994', 'robi@freemail.hu', 'felhasználó', 1);
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
